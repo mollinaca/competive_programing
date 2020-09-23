95b656442015d95255a2ed0b3dad890fc8e4fbe6
hstn <github@m.rel0.pw>
2020-09-23 20:59:11 +0900

    AC

---
+++ AtCoder/code/practice/abc/abc148/d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+n = int(input())
+l = list(map(int,input().split()))
+ans = 0
+x = 1
+for i in l:
+    if i == x:
+        x += 1
+    else:
+        ans += 1
+
+if ans == len(l):
+    print (-1)
+else:
+    print (ans)
