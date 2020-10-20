edd29dc7d7a62838002638507ce6be369a113994
mollinaca <dev@m.rel0.pw>
2020-10-20 23:21:59 +0900

    AC

---
+++ AtCoder/code/practice/other_contests/nikkei2019-qual/b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+n = int(input())
+a = input()
+b = input()
+c = input()
+
+ans = 0
+for x,y,z in zip(a,b,c):
+    ans += len(set([x,y,z]))-1
+
+print (ans)
