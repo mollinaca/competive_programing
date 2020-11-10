0b989860cb0a3d8a8cac8186eed60b20f1246367
mollinaca <dev@m.rel0.pw>
2020-11-11 00:35:30 +0900

    AC

---
+++ AtCoder/code/practice/arc/arc105/b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+n = int(input())
+l = list(map(int,input().split()))
+
+import math
+from functools import reduce
+
+def gcd_list(numbers:list) -> int:
+    return reduce(math.gcd, numbers)
+
+print (gcd_list(l))
+
