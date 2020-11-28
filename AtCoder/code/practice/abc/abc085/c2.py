7076900929ab1a5dd7e0e647ca51a11f1f7e26de
mollinaca <dev@m.rel0.pw>
2020-11-29 01:05:30 +0900

    AC

---
+++ AtCoder/code/practice/abc/abc085/c2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+n,y = map(int,input().split())
+
+for a in range((y//10000)+1):
+    for b in range((y//5000)+1):
+        c = n-(a+b)
+        if c < 0:
+            break
+        if 10000*a + 5000*b + 1000*c == y:
+            print (a,b,c)
+            exit ()
+
+print (-1,-1,-1)
