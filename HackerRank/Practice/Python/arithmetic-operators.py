a21dce24aa0aa4a454f505af9af3946a4c78c817
mollinaca <dev@m.rel0.pw>
2021-02-11 06:18:27 +0900

    AC

---
+++ HackerRank/Practice/Python/arithmetic-operators.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
---
+++ HackerRank/Practice/Python/division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# https://www.hackerrank.com/challenges/python-division/problem
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (a//b)
+    print (a/b)
